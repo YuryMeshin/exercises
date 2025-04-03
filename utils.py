5cf94d6446ca05f804fc3d0a53d83d404d50cba6
Yury Meshin <yury@localhost.localdomain>
2025-04-03 20:34:13 +0100

    update random variables operations

--- utils.py
+++ utils.py
@@ -35,11 +35,27 @@ class DiscreteRandomVariable():
                 chunks.append(template.format(self.domain[i], self.probability[i]))
         return ''.join(chunks)
     
+    def copy(self) -> Self:
+        return type(self)(self.domain, self.precision, self.probability, self.name)
+    
+    def __eq__(self, other: Union[Self, Number]) -> bool:
+        if isinstance(other, type(self)):
+            return (self.domain == other.domain) & (self.probability == other.probability)
+        elif isinstance(other, Number):
+            if len(self.domain) == 1:
+                return self.domain[0] == other
+            else:
+                return False
+        return NotImplemented
+    
+    def __ne__(self, other: Union[Self, Number]) -> bool:
+        result = self.__eq__(other)
+        return NotImplemented if result is NotImplemented else not result
+
     def __add__(self, other: Union[Self, Number]) -> Self:
         if isinstance(other, Number):
-            return DiscreteRandomVariable([d + other for d in self.domain], self.precision, \
-                                            self.probability, self.name)
-        elif isinstance(other, DiscreteRandomVariable):
+            return type(self)([d + other for d in self.domain], self.precision, self.probability, self.name)
+        elif isinstance(other, type(self)):
             n = len(self.domain)
             m = len(other.domain)
             precision = max(self.precision, other.precision)
@@ -48,14 +64,12 @@ class DiscreteRandomVariable():
                 new_val = round(self.domain[i] + other.domain[j], precision)
                 new_weight = self.probability[i] * other.probability[j]
                 values[new_val] = values.get(new_val, 0) + new_weight
-            return DiscreteRandomVariable(list(values.keys()), precision, \
-                                            values.values(), '+'.join([self.name, other.name]))
+            return type(self)(list(values.keys()), precision, values.values(), '+'.join([self.name, other.name]))
     
     def __sub__(self, other: Union[Self, Number]) -> Self:
         if isinstance(other, Number):
-            return DiscreteRandomVariable([d - other for d in self.domain], self.precision, \
-                                            self.probability, self.name)
-        elif isinstance(other, DiscreteRandomVariable):
+            return type(self)([d - other for d in self.domain], self.precision, self.probability, self.name)
+        elif isinstance(other, type(self)):
             n = len(self.domain)
             m = len(other.domain)
             precision = max(self.precision, other.precision)
@@ -64,14 +78,12 @@ class DiscreteRandomVariable():
                 new_val = round(self.domain[i] - other.domain[j], precision)
                 new_weight = self.probability[i] * other.probability[j]
                 values[new_val] = values.get(new_val, 0) + new_weight
-            return DiscreteRandomVariable(list(values.keys()), precision, \
-                                            values.values(), '-'.join([self.name, other.name]))
+            return type(self)(list(values.keys()), precision, values.values(), '-'.join([self.name, other.name]))
     
     def __mul__(self, other: Union[Self, Number]) -> Self:
         if isinstance(other, Number):
-            return DiscreteRandomVariable([d * other for d in self.domain], self.precision, \
-                                            self.probability, self.name)
-        elif isinstance(other, DiscreteRandomVariable):
+            return type(self)([d * other for d in self.domain], self.precision, self.probability, self.name)
+        elif isinstance(other, type(self)):
             n = len(self.domain)
             m = len(other.domain)
             precision = max(self.precision, other.precision)
@@ -80,16 +92,27 @@ class DiscreteRandomVariable():
                 new_val = round(self.domain[i] * other.domain[j], precision)
                 new_weight = self.probability[i] * other.probability[j]
                 values[new_val] = values.get(new_val, 0) + new_weight
-            return DiscreteRandomVariable(list(values.keys()), precision, \
-                                            values.values(), '*'.join([self.name, other.name]))
+            return type(self)(list(values.keys()), precision, values.values(), '*'.join([self.name, other.name]))
     
+    def __pow__(self, deg: int) -> Self:
+        assert isinstance(deg, int), 'Only non negative integer power is supported'
+        assert deg >= 0, 'Only non negative integer power is supported'
+        if deg == 0:
+            return 1
+        if deg == 1:
+            return self.copy()
+        values = {}
+        for i in range(len(self.domain)):
+            new_val = self.domain[i] ** deg
+            values[new_val] = values.get(new_val, 0) + self.probability[i]
+        return type(self)(list(values.keys()), self.precision, values.values(), f'{self.name}^{deg}')
+
     def __truediv__(self, other: Union[Self, Number]) -> Self:
         if isinstance(other, Number):
             if other == 0:
                 raise ZeroDivisionError('Given value of the divisor is equal to 0')
-            return DiscreteRandomVariable([d / other for d in self.domain], self.precision, \
-                                            self.probability, self.name)
-        elif isinstance(other, DiscreteRandomVariable):
+            return type(self)([d / other for d in self.domain], self.precision, self.probability, self.name)
+        elif isinstance(other, type(self)):
             if 0 in other.domain:
                 raise ZeroDivisionError(f'Given random variable {other.name} has value of 0 in its domain')
             n = len(self.domain)
@@ -100,5 +123,4 @@ class DiscreteRandomVariable():
                 new_val = round(self.domain[i] / other.domain[j], precision)
                 new_weight = self.probability[i] * other.probability[j]
                 values[new_val] = values.get(new_val, 0) + new_weight
-            return DiscreteRandomVariable(list(values.keys()), precision, \
-                                            values.values(), '/'.join([self.name, other.name]))
+            return type(self)(list(values.keys()), precision, values.values(), '/'.join([self.name, other.name]))
